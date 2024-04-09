e85f599fef375e56e9dd4385c2c4e994ad69d92c
Jithin KG <jithinjkg@gmail.com>
2024-04-09 16:20:21 +0530

    basic layout

--- pkg/ui/home/home_screen.go
+++ pkg/ui/home/home_screen.go
@@ -1,49 +1,48 @@
 package home
 
 import (
+	"fmt"
 	"log"
 	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"github.com/jithin-kg/GoTermPlus/pkg/ui/terminal"
 	"golang.org/x/crypto/ssh"
 )
 
 func NewHomeScreen(window fyne.Window) fyne.CanvasObject {
 	// form items
 	username := widget.NewEntry()
+	username.SetText("root")
 	password := widget.NewPasswordEntry()
+	password.SetText("root")
 	hostEntry := widget.NewEntry()
+	hostEntry.SetText("localhost:2222")
+	statusLabel := widget.NewLabel("")
+	statusLabel.Wrapping = fyne.TextWrapWord
 
 	sshKey := widget.NewButton("Select SSH key", func() {
 		// key selection logic
 	})
+
 	connectBtn := widget.NewButton("Connect", func() {
-		// Implement connection logic
-		config := ssh.ClientConfig{
-			User: username.Text,
-			Auth: []ssh.AuthMethod{
-				ssh.Password(password.Text),
-			},
-			HostKeyCallback: ssh.InsecureIgnoreHostKey(), //NOTE: not recommented for production, just for testing
-			Timeout:         5 * time.Second,
+		// connection logic
+		config := CreateSShClientConfig(username.Text, password.Text)
+		details := &SSHConnectionDetails{
+			Config:  config,
+			Address: hostEntry.Text,
+			Status:  statusLabel,
 		}
-		client, err := ssh.Dial("tcp", hostEntry.Text, &config)
-		if err != nil {
-			log.Println("Failed to dial: ", err)
-			// window.Canvas().Refresh(formContainer) // update the ui to show any error message
-			return
+		details.OnSuccess = func(client *ssh.Client) {
+			fyne.CurrentApp().SendNotification(&fyne.Notification{
+				Title:   "Connection Established",
+				Content: "Ssh connection established sucessfully.",
+			})
+			window.SetContent(terminal.NewTerminalScreen(window, client))
 		}
-		session, err := client.NewSession()
-		if err != nil {
-			log.Println("Failed to create session:", err)
-			client.Close()
-			// window.Canvas().Refresh(formContainer)
-			return
-		}
-		defer session.Close()
-		log.Println("SSH connection established")
+		go connectToSSH(details)
 
 	})
 
@@ -53,14 +52,51 @@ func NewHomeScreen(window fyne.Window) fyne.CanvasObject {
 		widget.NewFormItem("Username", username),
 		widget.NewFormItem("Password", password),
 	)
-	form.OnSubmit = func() {
-		// Implement the form submission logic here
-	}
-	formContainer := container.NewVBox(form, sshKey, connectBtn)
+	form.OnSubmit = connectBtn.OnTapped //bind the form submission to the button's tap handler
+	formContainer := container.NewVBox(form, sshKey, connectBtn, statusLabel)
 	// return container with form and submit button
 	return formContainer
 }
 
-func onConnectClick() {
+func CreateSShClientConfig(username, password string) *ssh.ClientConfig {
+	return &ssh.ClientConfig{
+		User: username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), //NOTE: not recommented for production, just for testing
+		Timeout:         5 * time.Second,
+	}
+}
 
+type SSHConnectionDetails struct {
+	Config    *ssh.ClientConfig
+	Address   string
+	Status    *widget.Label
+	Client    *ssh.Client
+	OnSuccess func(client *ssh.Client) // Callback function
+}
+
+func connectToSSH(details *SSHConnectionDetails) {
+	client, err := ssh.Dial("tcp", details.Address, details.Config)
+	if err != nil {
+		log.Println("Failed to dial: ", err)
+		details.Status.SetText(fmt.Sprintf("Failed to dial: %s", err))
+		return
+	}
+	session, err := client.NewSession()
+	if err != nil {
+		log.Println("Failed to create session:", err)
+		client.Close()
+		details.Status.SetText(fmt.Sprintf("Failed to create session: %s", err))
+		return
+	}
+	defer session.Close()
+	details.Client = client
+	if details.OnSuccess != nil {
+		details.OnSuccess(client)
+	}
+	details.Status.SetText("SSH connection established")
+
+	log.Println("SSH connection established")
 }
--- pkg/ui/terminal/terminal_screen.go
+++ pkg/ui/terminal/terminal_screen.go
@@ -1,40 +1,91 @@
 package terminal
 
 import (
+	"fmt"
+	"log"
+	"strings"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+	"golang.org/x/crypto/ssh"
 )
 
-func NewTerminalScreen(window fyne.Window) fyne.CanvasObject {
+func NewTerminalScreen(window fyne.Window, client *ssh.Client) fyne.CanvasObject {
+	// list directories and files
+	listItems, err := listDirectories(client, "/")
+	if err != nil {
+		log.Printf("Failed to list directories %v", err)
+	}
+
 	// file browser pane
-	fileBrowser := widget.NewList(func() int {
+	fileBrowser := setupFileBrowser(listItems)
+	fileBrowserScroll := container.NewScroll(fileBrowser)
+
+	container.NewScroll(fileBrowser)
+	// create terminal pane
+	terminal := setupTerminal()
+	terminalScroll := container.NewVScroll(terminal)
+	terminalScroll.SetMinSize(fyne.NewSize(400, 300)) // Set a minimum size for the terminal pane
+
+	// top panel with options and settings
+	topPanel := container.NewHBox(
+		widget.NewLabel("Options:"),
+		widget.NewButtonWithIcon("Copy", theme.ContentCopyIcon(), func() {}),
+		// Add more toolbar actions as needed
+		layout.NewSpacer(), // Pushes everything to the left
+	)
+	fmt.Println(topPanel)
+	// assemble screen using horizontal split container
+	hSplit := container.NewHSplit(fileBrowserScroll, terminalScroll)
+	hSplit.Offset = 0.25 // initial split ratio
+	// hSplit.StretchRight
+	// return container.New(layout.NewGridLayout(2), fileBrowserScroll, terminalScroll)
+	return container.NewBorder(topPanel, nil, nil, hSplit)
+}
+func setupTerminal() *widget.Entry {
+	// Create terminal pane with multiline entry
+	terminal := widget.NewMultiLineEntry()
+	terminal.MultiLine = true // Enable multiline
+	terminal.SetPlaceHolder("Terminal will be here")
+
+	return terminal
+}
+func setupFileBrowser(listItems []string) *widget.List {
+	fileItem := widget.NewLabel("")
+	fileItem.Wrapping = fyne.TextWrap(fyne.TextTruncateClip)
+
+	fb := widget.NewList(func() int {
 		// returns the number of items in the list
-		return 0
+		return len(listItems)
 	},
 		func() fyne.CanvasObject {
 			// return the list item UI
-			return nil
+			return container.NewHBox(fileItem, layout.NewSpacer())
 		},
 		func(id widget.ListItemID, obj fyne.CanvasObject) {
 			// update the list item UI
+			label := obj.(*fyne.Container).Objects[0].(*widget.Label)
+			label.SetText(listItems[id]) //set the lable text to directory/ file name
 		},
 	)
-	// create terminal pane
-	terminal := widget.NewMultiLineEntry()
-	terminal.SetPlaceHolder("Terminal will be here")
 
-	// top panel with options and settings
-	topPanel := widget.NewToolbar(
-		widget.NewToolbarAction(theme.ContentCopyIcon(), func() {
-			// implement copy functionality
-		}),
-		// additional toolbar items
-	)
-
-	// assemble screen using horizontal split container
-	hSplit := container.NewHSplit(fileBrowser, terminal)
-	hSplit.Offset = 0.3 // initial split ratio
-	return container.NewBorder(topPanel, nil, nil, hSplit)
+	return fb
+}
+func listDirectories(client *ssh.Client, path string) ([]string, error) {
+	session, err := client.NewSession()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create session: %w", err)
+	}
+	defer session.Close()
+	// command to list directories and files
+	cmd := fmt.Sprintf("ls -l %s", path)
+	ouput, err := session.CombinedOutput(cmd)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute command %s: %w", cmd, err)
+	}
+	lines := strings.Split(string(ouput), "\n")
+	return lines, nil
 }
